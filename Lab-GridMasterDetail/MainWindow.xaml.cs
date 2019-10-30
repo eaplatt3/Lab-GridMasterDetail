9e61e101eeda2661c83aa74b670bb29007d68625
SOB Lab <soblab@it.farmingdale.edu>
2019-10-30 10:34:47 -0400

    Add project files.

---
+++ Lab-GridMasterDetail/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_GridMasterDetail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            fileName = "TestData.txt";
+
+            Movie m; 
+
+            StreamReader read = new StreamReader(fileName);
+
+            while (!read.EndOfStream)
+            {
+               m = new Movie();
+
+                m.Name = read.ReadLine();
+
+                MasterList.Items.Add(m.ToString());
+            
+            }
+        }
+    }
+}
---
+++ Lab-GridMasterDetail/Movie.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_GridMasterDetail
+{
+    class Movie
+    {
+        string mName;
+        int mRottenTomatosScore;
+        string mReview;
+
+        public string Name
+        {
+            get
+            {
+                return mName;
+            }
+
+            set
+            {
+                mName = value;
+            }
+        }
+
+        public int RottenTomatosScore
+        {
+            get
+            {
+                return mRottenTomatosScore;
+            }
+
+            set
+            {
+                mRottenTomatosScore = value;
+            }
+        }
+
+        public string Review
+        {
+            get
+            {
+                return mReview;
+            }
+
+            set
+            {
+                mReview = value;
+            }
+        }
+
+        public Movie()
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+    }
+}
